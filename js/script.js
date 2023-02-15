6208f108454065c05849a8bf52d17852e3c5cc00
Matusalem Gabriel <matu.dev25@gmail.com>
2023-02-15 08:00:27 -0300

    Commit1 15.02

--- js/script.js
+++ js/script.js
@@ -1,2 +1,15 @@
-function myFunction(x) {
-    x.classList.toggle("change");}
+const menuBtn = document.querySelector('.menu-btn');
+const menuDesc = document.querySelector('.menu-description')
+let menuOpen = false;
+menuBtn.addEventListener('click', () => {
+  if(!menuOpen) {
+    menuBtn.classList.add('open');
+
+    menuDesc.style.display = 'block'
+    menuOpen = true;
+  } else {
+    menuBtn.classList.remove('open');
+    menuDesc.style.display = 'none'
+    menuOpen = false;
+  }
+});
