86486324b1e615bfae76b13f075ff0a98ae6e6d0
Matusalem Gabriel <122733420+Markyz25@users.noreply.github.com>
2023-02-23 23:35:22 -0300

    Att2 23.02

--- js/script.js
+++ js/script.js
@@ -8,3 +8,32 @@ function showMenu() {
     let imgMenu = document.querySelector('.icon').src = 'assets/menuClose.svg'
   }
 }
+
+const pokeName = document.querySelector('.pokemon__name')
+const pokeNumber = document.querySelector('.pokemon__number')
+const pokeImage = document.querySelector('.pokemon__image')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('#search-pokemon')
+
+const fetchPokemon = async (pokemon) => {
+
+  const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  const data = await apiResponse.json()
+  return data
+}
+
+const renderPokemon = async (pokemon) => {
+  
+  const data = await fetchPokemon(pokemon);
+  pokeName.innerHTML = data.name
+  pokeNumber.innerHTML = data.id
+  pokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  renderPokemon(input.value)
+})
+
