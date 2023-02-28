d0509614674b99ce7422f15e6e200f0865d4fea5
Matusalem Gabriel <matu.dev25@gmail.com>
2023-02-27 23:22:30 -0300

    Att 27.02

--- js/script.js
+++ js/script.js
@@ -1,10 +1,45 @@
+
 function showMenu() {
-  let menuMobile = document.querySelector('.mobile-menu')
-  if (menuMobile.classList.contains('open')) {
-    menuMobile.classList.remove('open');
-    let imgMenu = document.querySelector('.icon').src = 'assets/menu.svg'
+  let nav = document.querySelector('.nav-list')
+  let teste = document.querySelectorAll('.teste')
+  if (nav.classList.contains('open')) {
+    nav.classList.remove('open');
+    teste.forEach(function(testes) {
+      testes.classList.remove('fixed')
+    })
   } else {
-    menuMobile.classList.add('open')
-    let imgMenu = document.querySelector('.icon').src = 'assets/menuClose.svg'
+    nav.classList.add('open')
+    teste.forEach(function(testes) {
+    testes.classList.add('fixed')
+    })
   }
 }
+
+const pokeName = document.querySelector('.pokemon__name')
+const pokeNumber = document.querySelector('.pokemon__number')
+const pokeImage = document.querySelector('.pokemon__image')
+
+const form = document.querySelector('.form')
+const input = document.querySelector('#search-pokemon')
+
+const fetchPokemon = async (pokemon) => {
+
+  const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+  const data = await apiResponse.json()
+  return data
+}
+
+const renderPokemon = async (pokemon) => {
+  
+  const data = await fetchPokemon(pokemon);
+  pokeName.innerHTML = data.name
+  pokeNumber.innerHTML = data.id
+  pokeImage.src = data['sprites']['versions']['generation-v']['black-white']['animated']['front_default']
+}
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+
+  renderPokemon(input.value)
+})
+
