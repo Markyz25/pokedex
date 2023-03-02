9c249985c8bea9c2d73869c119245db39b5ff9ac
Matusalem Gabriel <122733420+Markyz25@users.noreply.github.com>
2023-03-01 23:47:53 -0300

    Melhorias

--- js/script.js
+++ js/script.js
@@ -23,27 +23,64 @@ const pokeImage = document.querySelector(".pokemon__image");
 
 const form = document.querySelector(".form");
 const input = document.querySelector("#search-pokemon");
+const btnPrev = document.querySelector(".btn-prev")
+const btnNext = document.querySelector(".btn-next")
+
+let searchPokemon = 1
 
 const fetchPokemon = async (pokemon) => {
   const apiResponse = await fetch(
     `https://pokeapi.co/api/v2/pokemon/${pokemon}`
   );
-  const data = await apiResponse.json();
-  return data;
+
+  if (apiResponse.status === 200) {
+    const data = await apiResponse.json();
+    return data
+  }
+  ;
 };
 
 const renderPokemon = async (pokemon) => {
+
+  pokeName.innerHTML = 'Loading...'
+  pokeNumber.innerHTML = ''
   const data = await fetchPokemon(pokemon);
-  pokeName.innerHTML = data.name;
-  pokeNumber.innerHTML = data.id;
-  pokeImage.src =
+  
+  if (data) {
+    pokeImage.style.display = 'block'
+    pokeName.innerHTML = data.name;
+    pokeNumber.innerHTML = data.id;
+    pokeImage.src =
     data["sprites"]["versions"]["generation-v"]["black-white"]["animated"][
       "front_default"
     ];
+    input.value = ''
+    searchPokemon = data.id
+  } else {
+    pokeName.innerHTML = 'Not found'
+    pokeNumber.innerHTML = ''
+    pokeImage.style.display = 'none'
+  }
+  
 };
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
-
-  renderPokemon(input.value);
+  renderPokemon(input.value.toLowerCase());
 });
+
+btnPrev.addEventListener("click", () => {
+  if (searchPokemon > 1) {
+    searchPokemon -= 1
+    renderPokemon(searchPokemon)
+  }
+  
+})
+
+btnNext.addEventListener("click", () => {
+  searchPokemon += 1
+  renderPokemon(searchPokemon)
+})
+
+renderPokemon(searchPokemon)
+
